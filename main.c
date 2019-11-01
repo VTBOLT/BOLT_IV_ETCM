d3636eb9417e47bfe7a83daa30c7a277cd6e4252
Quinton <qemiller@vt.edu>
2019-11-01 09:49:57 -0400

    Switching changes that Josh ment for the master branch into the master branch, these changes are from the commit on position sensor with comment 'added new function prototypes, extended init function, added a requestTorque function that by defaul requests 0 torque.

--- main.c
+++ main.c
@@ -1,9 +1,73 @@
-
-
 /**
  * main.c
  */
+
+#include "driverlib.h"
+#include "device.h"
+
+//Function Prototypes.
+void init(void);
+void run(void);
+void initLookup(void);
+void initADC(void);
+void initEPWM(void);
+void initADCSOC(void);
+void requestTorque(int torque);
+__interrupt void adcA0ISR(void);
+
+//Main, calls init and run
 int main(void)
 {
-	return 0;
+    init();
+    run();
+    return 0;
+}
+
+//Initialize, runs all initialization functions
+void init(void)
+{
+    initLookUp();
+    initADC();
+    initEPWM();
+    initADCSOC();
+}
+
+//Main loop
+void run(void)
+{
+    int torque_request;
+    while(true)
+    {
+        torque_request = 0;
+        //check sensors
+
+        //check lookup table
+
+        //do any neccesary calculations
+
+        //send torque request to motor
+        requestTorque(torque_request);
+    }
+}
+
+//Initialize lookup tables
+void initLookup(void)
+{
+    //open file containing tables
+
+    //load tables into rom
+}
+
+//Initialize and power up the ADC
+void initADC(void)
+{
+    //initialize the ADC.
+}
+
+
+
+//send torque request to motor controller
+void requestTorque(int torque)
+{
+
 }
