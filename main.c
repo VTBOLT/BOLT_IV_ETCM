8961e502f3ea7061c6616518e3433a40c00cdcdf
Will Campbell <wtc@vt.edu>
2019-11-01 14:58:59 -0400

    Splits into more organized code structure, adds CPU1_RAM to gitignore

--- main.c
+++ main.c
@@ -1,97 +1,65 @@
 //#############################################################################
 //
-// FILE:   empty_driverlib_main.c
+// FILE:   main.c
 //
-// TITLE:  Empty Project
-//
-// Empty Project Example
-//
-// This example is an empty project setup for Driverlib development.
+// TITLE:  BOLT IV ETCM
 //
 //#############################################################################
-// $TI Release: F2837xD Support Library v3.05.00.00 $
-// $Release Date: Tue Jun 26 03:15:23 CDT 2018 $
-// $Copyright:
-// Copyright (C) 2013-2018 Texas Instruments Incorporated - http://www.ti.com/
+// Contributors:
+//  Will Campbell
+//  Quinton Miller
+//  Josh Collins
 //
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions 
-// are met:
-// 
-//   Redistributions of source code must retain the above copyright 
-//   notice, this list of conditions and the following disclaimer.
-// 
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the 
-//   documentation and/or other materials provided with the   
-//   distribution.
-// 
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// $
+//#############################################################################
+//Notes:
+//  Follow the GNOME style guide
+//
+//  Anything related to individual peripherals should be in corresponding .c and .h files
+//      that are then included below
+//
+//
 //#############################################################################
 
-//
-// Included Files
-//
-#include "driverlib.h"
-#include "device.h"
+// Included peripheral files
+#include "adc_etcm.h"
 
 //Function Prototypes.
 void init(void);
 void run(void);
 void initLookup(void);
-void initADC(void);
-void initEPWM(void);
-void initADCSOC(void);
 void requestTorque(int torque);
-__interrupt void adcA0ISR(void);
 
-//
-// Main
-//
 void main(void)
 {
     init();
     run();
-    return 0;
+
+    return;
 }
 
 void run(void)
 {
-    char i = 0;
-    char torque_request = 0;
+    int torque_request = 0; // likely to change type
+
     while (1)
     {
-        //check sensors
+        // Pull in sensor data to local variables
+        // This will use getters inside the peripheral .h/.c files
 
-        //check lookup table
+        // Follow lookup table logic
+        // Specific logic TBD
 
-        //do any neccesary calculations
+        // Carry out any calculations
 
-        //send torque request to motor
+        // Send torque request to motor
         requestTorque(torque_request);
-        i = i + 1;
     }
 }
 
 //Initialize, runs all initialization functions
 void init(void)
 {
-    initLookUp();
+    initLookup();
     initADC();
     initEPWM();
     initADCSOC();
@@ -100,45 +68,12 @@ void init(void)
 //Initialize lookup tables
 void initLookup(void)
 {
-    //open file containing tables
+    // Open file containing tables
 
-    //load tables into rom
+    // Load tables into ROM
 }
 
-//Initialize and power up the ADC
-void initADC(void)
-{
-    //Set ADC divider to /4.
-    ADC_setPrescaler(ADCA_BASE, ADC_CLK_DIV_4_0);
 
-    //Set resolution and signal mode and load the right trims.
-    ADC_setMode(ADCA_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
-
-    //Set pulse position to late.
-    ADC_setInterruptPulseMode(ADCA_BASE, ADC_PULSE_END_OF_CONV);
-
-    //Power up ADC and delay for one ms.
-    ADC_enableConverter(ADCA_BASE);
-    DEVICE_DELAY_US(1000);
-}
-
-//Initialize EPWM
-void initEPWM(void)
-{
-
-}
-
-//Initialize ADC SOC
-void initADCSOC(void)
-{
-
-}
-
-//send torque request to motor controller
-void requestTorque(int torque)
-{
-
-}
 
 //
 // End of File
