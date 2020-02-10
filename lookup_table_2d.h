9de7ee8b9cf571a884f0629348ae24453f13890c
Stephen Welch <stephenwelchva@gmail.com>
2020-02-10 08:36:00 -0500

    Create lookup table header files

---
+++ lookup_table_2d.h
@@ -0,0 +1,14 @@
+/**
+ * lookup_table_2d.h
+ *
+ *  Created on: Feb 10, 2019
+ *      Author: Stephen Welch
+ */
+
+#ifndef LOOKUP_TABLE_2D_H_
+#define LOOKUP_TABLE_2D_H_
+
+void init();
+float get(float value);
+
+#endif
---
+++ lookup_table_3d.h
@@ -0,0 +1,14 @@
+/**
+ * lookup_table_3d.h
+ *
+ *  Created on: Feb 10, 2019
+ *      Author: Stephen Welch
+ */
+
+#ifndef LOOKUP_TABLE_3D_H_
+#define LOOKUP_TABLE_3D_H_
+
+void init();
+float get(float x, float y);
+
+#endif
