f73fc19ff204bdc673aca0f06384d3c8cfd40d80
Stephen Welch <stephenwelchva@gmail.com>
2020-02-10 22:48:30 -0500

    Add basic interpolation

--- interpolation_lookup_table_2d.c
+++ interpolation_lookup_table_2d.c
@@ -23,8 +23,15 @@ void init(const lookup_table_2d_t& table) {
 
 float get(const lookup_table_2d_t& table, float value) {
     // Find approximate index
-    // If approximate index isn't actual location, linear search
-    // Return value
+    float approx_index = (value - table.min_value) / (table.max_value - table.min_value) * (LOOKUP_TABLE_2D_SIZE - 1);
+    // The approximate index will be a decimal value - so find its 2 integer neighbors
+    int index_floor = floorf(approx_index);
+    int index_ceil = ceilf(approx_index);
+
+    // Interpolate between the value from the lower index and the value from the higher index.
+    float interpolated_value = interpolate(table.values[index_floor], table.values[index_ceil], approx_index / (index_ceil - index_floor));
+
+    return interpolated_value;
 }
 
 
