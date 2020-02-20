767d4bb60159dc41dd0519920bac795d79cb5652
Stephen Welch <stephenwelchva@gmail.com>
2020-02-19 19:35:05 -0500

    Implement utils in-header

--- util.h
+++ util.h
@@ -11,11 +11,19 @@
 /**
  * Interpolates between a start value and end value based on an amount from 0-1.
  */
-float interpolate(float start, float end, float amount);
+float interpolate(float start, float end, float amount) {
+    return start + ((end - start) * amount);
+}
 
 /**
  * Checks if a and b are within a amount of each other.
  */
-bool epsilon_equals(float eps, float a, float b);
+bool epsilon_equals(float eps, float a, float b) {
+    if(a - b < eps || b - a < eps) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 #endif /* UTIL_H_ */
