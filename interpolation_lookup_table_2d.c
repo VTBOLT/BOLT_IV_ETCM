43b876739b887b60f7af3139cd7405b621308d62
Stephen Welch <stephenwelchva@gmail.com>
2020-02-19 19:41:51 -0500

    Cleanup and comment

--- interpolation_lookup_table_2d.c
+++ interpolation_lookup_table_2d.c
@@ -25,19 +25,18 @@ float get(const lookup_table_2d_t& table, float value) {
     // Find approximate index
     float approx_index = (value - table.min_value) / (table.max_value - table.min_value) * (LOOKUP_TABLE_2D_SIZE - 1);
     // The approximate index will be a decimal value - so find its 2 integer neighbors
-    int approx_index_floor = floorf(approx_index);
-    int approx_index_ceil = ceilf(approx_index);
+    int search_index_floor = floorf(approx_index);
+    int search_index_ceil = ceilf(approx_index);
 
-    // Store the indices we're searching separately;
-    // We need the original indices as a reference so we can search left and right simultaneously
-    int search_index_floor = approx_index_floor;
-    int search_index_ceil = approx_index_ceil;
     bool found = false;
 
     while(!found) {
+        // Check if value is between two values in the table
         if(value >= table.values[search_index_floor] && value <= table.values[search_index_ceil]) {
             found = true;
+        // Since the lookup table is ordered, we can decide whether to iterate left or right
         } else if(value > table.values[search_index_floor]) {
+            // If we're going out of bounds, just use the last value in the lookup table
             if(search_index_ceil + 1 >= LOOKUP_TABLE_2D_SIZE) {
                 found = true;
             } else {
@@ -53,6 +52,7 @@ float get(const lookup_table_2d_t& table, float value) {
             }
         }
     }
+    // Recalculate the approximate index by interpolating by two nearest values
     approx_index = (value - table.values[search_index_floor]) / (table.values[search_index_ceil] - table.values[search_index_floor]) * (LOOKUP_TABLE_2D_SIZE - 1);
     // Interpolate between the value from the lower index and the value from the higher index.
     float interpolated_value = interpolate(table.values[search_index_floor], table.values[search_index_ceil], approx_index / (search_index_ceil - search_index_floor));
