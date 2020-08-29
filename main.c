eb128d57745fbc3ad78f5ac706a580f52d25b23d
Will Campbell <wtc@vt.edu>
2020-08-29 16:50:19 -0400

    Simplified for 2020-21, see @todo comments. Good luck!

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
-//#############################################################################
-//
-// FILE:   main.c
-//
-// TITLE:  BOLT IV ETCM
-//
-//#############################################################################
-// Contributors:
-//  Will Campbell
-//  Quinton Miller
-//  Josh Collins
-//  Tyler Shaffer
-//#############################################################################
-// Tyler Shaffer, 1/8/2020: Imported can_etcm.h, noticed that a new version of
-// C2000ware exists also.
-// Tyler Shaffer, 1/8/2020: Checked CAN_A pins.
-//
-//#############################################################################
-//Notes:
-//  Follow the GNOME style guide
-//
-//  Anything related to individual peripherals should be in corresponding .c and .h files
-//      that are then included below
-//
-//
-//#############################################################################
+/******************************************************************************
+*
+* FILE:   main.c
+*
+* TITLE:  BOLT IV ETCM
+*
+*******************************************************************************
+* Contributors:
+*  Will Campbell
+*  Quinton Miller
+*  Josh Collins
+*  Tyler Shaffer
+*******************************************************************************
+* Tyler Shaffer, 1/8/2020: Imported can_etcm.h, noticed that a new version of
+* C2000ware exists also.
+* Tyler Shaffer, 1/8/2020: Checked CAN_A pins.
+* Will Campbell, 8/29/20: Commented out extraneous code, added notes. I haven't
+* worked on this code in a while so no guarantees, it will still need to be added to
+* and extensively tested, but the goal (as I see it) is to make BOLT IV a functioning
+* bike that functions via fly-by-wire (electronic transmission of throttle signal), but
+* no traction control because that will take much more research and testing. This will
+* be raw throttle in to raw throttle out. The things that I can see will need to be
+* done will be marked by @todo.
+*
+*******************************************************************************
+* Notes:
+*  Follow the GNOME style guide
+*
+*  Anything related to individual peripherals should be in corresponding .c and .h files
+*      that are then included below
+*
+*******************************************************************************/
 
 // Included peripheral files
 #include <can_etcm.h>
@@ -56,46 +62,54 @@ void run(void)
     {
         // Pull in sensor data to local variables
         // This will use getters inside the peripheral .h/.c files
+
+        // @todo - implement ADC and write getThrottle() method. Verify values with separate ADC and ensure full range of input works
+        //         This could probably be done by extending Quinton's ADC code in the position_sensor branch
         float rawThrottleIn = 0; // getThrottle()
+
+        torque_request = rawThrottleIn; // unmodified
+
+        /* No TC, unnecessary
         int rawFrontSusp = 0; // getFrontSusp()
         int rawBackSusp = 0; // getBackSusp()
+        *
+        *
+        * Follow lookup table logic
+        * Specific logic TBD
+        *
+        * The basic structure will be as follows:
+        *      First, check which TC categories need to be calculated/looked up
+        *      Then, do those calculations and store the results
+        *      Look at all the results and find the largest reduction
+        *      Move forward with that
+        *
+        *
+        *bool checks[4]; // raw_in, traction_control, anti_wheelie, anti_jerk
+        *
+        *checks[0] = true; // raw_in will always qualify
+        *
+        *
+        * Carry out any calculations
+        */
 
-        // Follow lookup table logic
-        // Specific logic TBD
-
-        /* The basic structure will be as follows:
-         *      First, check which TC categories need to be calculated/looked up
-         *      Then, do those calculations and store the results
-         *      Look at all the results and find the largest reduction
-         *      Move forward with that
-         */
-
-        bool checks[4]; // raw_in, traction_control, anti_wheelie, anti_jerk
-
-        checks[0] = true; // raw_in will always qualify
-
-
-
-
-
-        // Carry out any calculations
-
-
-
-
-        // Send torque request to motor
-        requestTorque(torque_request);
+        // Send torque request to motor - this will be the raw torque request, unmodified
+        requestTorque(torque_request); // @todo - verify functionality in DAC file over full range of output values, Stephen wrote this and it's merged
 
         // Send a test CANmsg
-        //CANtest();
+        /* @todo - you'll still want CAN messages out of this so that the CANlogger can pick
+         * them up and you can record and verify that IO throttle matches up. See can_etcm.h
+         * and can_etcm.c, written by Tyler. For any further functionality, implement from
+         * the CAN examples. They can be finicky and you should make sure there aren't any
+         * jumpers or other hardware on the silkscreen that need to be arranged in a certain
+         * way, but the examples will work. Also, consider buying a CAN analyzer.
+        //CANtest()
+         * @todo - verify test works, then put in actual data
+         */
 
-        // Flash the blue LED
+        // Flash the blue LED - this is just debug/verify the board is actually functioning
         LEDflash();
 
-        // uart test
-        //SCItest();
-
-       getIMUdata();
+        getIMUdata(); // @todo - this is Quinton's code, I'm fairly sure that it works, still verify
 
     }
 }
@@ -110,25 +124,29 @@ void init(void)
     Device_init();
     initGPIO();     // do not move
 
-    initLookup();
-    //initADC();
-    //initEPWM();
-    //initADCSOC();
+    //initLookup(); // removed, no lookup table
+    initADC();
+    //initEPWM();   // should be removed, only TC-required
+    //initADCSOC(); // @todo - honestly I don't remember what this was for. Aux bat SOC is measured
+                    // on MSP432 (and should be rewritten), check on PCB. Check with Quinton if you
+                    // can't figure it out
     initCAN();
-    //initSCI();
+    //initSCI();    // @todo - figure out which or both initSCI methods are necessary
     initSCIFIFO();
     initDAC();
 }
 
-//Initialize lookup tables
-void initLookup(void)
-{
-    // Open file containing tables
-
-    // Load tables into ROM
-}
-
+//Initialize lookup tables (should be unused)
 /*
+ *  void initLookup(void)
+ * {
+ *   // Open file containing tables
+ *
+ *   // Load tables into ROM
+ * }
+
+
+
  * Send out a test message over CAN
  * ID: 0x401
  */
@@ -205,9 +223,9 @@ void getIMUdata(){
         // check amount of data bytes received
         if (dataIndex >= IMU_FRAME_SIZE){
             // send buffer data over CAN
-            CANA_transmitMsg(dataBuffer, 8, 1);
-            CANA_transmitMsg(dataBuffer+8, 8, 2);   // increment pointer
-            CANA_transmitMsg(dataBuffer+16, 2, 3);   // increment pointer
+            //CANA_transmitMsg(dataBuffer, 8, 1);
+            //CANA_transmitMsg(dataBuffer+8, 8, 2);   // increment pointer
+            //CANA_transmitMsg(dataBuffer+16, 2, 3);   // increment pointer
             return;
         }
         // loop again
