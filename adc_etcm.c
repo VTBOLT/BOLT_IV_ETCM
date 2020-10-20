b6cdd352f1b3362feb8294f634452e5abf6a3572
Stephen Welch <stephenwelchva@gmail.com>
2020-10-20 17:00:33 -0400

    First pass at throttle ADC reads

--- adc_etcm.c
+++ adc_etcm.c
@@ -11,27 +11,45 @@
 void initADC(void)
 {
     //Set ADC divider to /4.
-    ADC_setPrescaler(ADCA_BASE, ADC_CLK_DIV_4_0);
+    ADC_setPrescaler(ADC_BASE_ADDRESS, ADC_CLK_DIV_4_0);
 
     //Set resolution and signal mode and load the right trims.
-    ADC_setMode(ADCA_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
+    ADC_setMode(ADC_BASE_ADDRESS, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
 
     //Set pulse position to late.
-    ADC_setInterruptPulseMode(ADCA_BASE, ADC_PULSE_END_OF_CONV);
+    ADC_setInterruptPulseMode(ADC_BASE_ADDRESS, ADC_PULSE_END_OF_CONV);
 
     //Power up ADC and delay for one ms.
-    ADC_enableConverter(ADCA_BASE);
+    ADC_enableConverter(ADC_BASE_ADDRESS);
     DEVICE_DELAY_US(1000);
-}
-
-//Initialize EPWM
-void initEPWM(void)
-{
-
 }
 
 //Initialize ADC SOC
 void initADCSOC(void)
 {
+    ADC_setupSOC(ADC_BASE_ADDRESS, ADC_SOC_NUMBER0, ADC_TRIGGER_SW_ONLY,
+                         ADC_CHANNEL, 15);
 
+    ADC_setInterruptSource(ADC_BASE_ADDRESS, ADC_INT_NUMBER1, ADC_SOC_NUMBER0);
+    ADC_enableInterrupt(ADC_BASE_ADDRESS, ADC_INT_NUMBER1);
+    ADC_clearInterruptStatus(ADC_BASE_ADDRESS, ADC_INT_NUMBER1);
 }
+
+uint16_t getADCRawValue() {
+    // Force read
+    ADC_forceSOC(ADC_BASE_ADDRESS, ADC_SOC_NUMBER0);
+    // Block until interrupt triggers to signal read completion
+    while(ADC_getInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1) == false)
+    {
+    }
+    // Clear the interrupt immediately
+    ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+
+    // Return ADC result
+    return ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
+}
+
+double getThrottleVoltage(void) {
+    return 0.0;
+}
+
--- adc_etcm.h
+++ adc_etcm.h
@@ -11,10 +11,15 @@
 #include "adc.h"
 #include "device.h"
 
+#define ADC_BASE_ADDRESS ADCB_BASE
+#define ADC_CHANNEL ADC_CH_ADCIN5
+
 void initADC(void);
-void initEPWM(void);
+//void initEPWM(void);
 void initADCSOC(void);
-__interrupt void adcA0ISR(void);
+uint16_t getADCRawValue(void);
+double getThrottleVoltage(void);
+//__interrupt void adcA0ISR(void);
 
 
 #endif /* ADC_ETCM_H_ */
