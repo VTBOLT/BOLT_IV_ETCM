cdfbd2a98f607f492d492fb9e047a284517e8f48
Derek Bruce <dbruce232@localhost.localdomain>
2021-04-04 16:35:15 -0400

    Adding constants

--- csvGenerator.py
+++ csvGenerator.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, random
+
+straight = [150,150,6,6,0,0,1,1,1,0,0,0,0,3,0,1]
+accelerating = [110,110,6,6,0,0,1,1,1,1,0,0,0,3,0,2]
+left = [80,90,4,8,1,0,0,1,1,0,17,0,35,16,0,36]
+slowing = [45,50,6,6,1,1,1,1,1,0,0,1,0,3,1,2]
+right = [75,85,5,7,1,0,1,1,0,0,20,0,155,18,0,153]
+
 
 def main(args):
-	print(args)
+	n = args[0]
+	for (i in range(n)):
+		
 
 
 
