d3943ff8f540749ccd11f28335c41d9e5a97d4f1
jennakrebs <jennakrebs@vt.edu>
2021-04-04 17:39:30 -0400

    Updated csv automatic populator

--- csvGenerator.py
+++ csvGenerator.py
@@ -10,13 +10,37 @@ right = [75,85,5,7,1,0,1,1,0,0,20,0,155,18,0,153]
 
 
 def main(args):
-	n = args[0]
-	for (i in range(n)):
+	n = int(args[0])
+	instructions = args[1:]
+	result = populate_directions(instructions)
+	for i in range(n):
+		partition = n / len(result)
+		count = int (i / partition)
+		for index in range(0, len(result[count])):
+			error = round(result[count][index] + random.uniform(-0.05 * result[count][index], 0.05 * result[count][index]), 3)
+			if index == len(result[count]) - 1:
+				print(error)
+			else:
+				print(error, end = ", ")
+
+
+def populate_directions(directions):
+	to_return = []
+	for i in range(len(directions)):
+		if directions[i] == "straight":
+			to_return.append(straight)
+		elif directions[i] == "accelerating":
+			to_return.append(accelerating)
+		elif directions[i] == "left":
+			to_return.append(left)
+		elif directions[i] == "slowing":
+			to_return.append(slowing)
+		elif directions[i] == "right":
+			to_return.append(right)
+		else:
+			sys.exit("You did not input a valid direction")
 		
-
-
-
-
+	return to_return
 
 if __name__ == "__main__":
     main(sys.argv[1:])
